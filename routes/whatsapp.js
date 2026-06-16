0e9bb6a844c69dfcc95817e3f1a492e753d2ab8d
sirsona <swaleh489@gmail.com>
2026-06-16 21:02:51 +0300

    feat: add Whatsapp webhook verification enpoint

---
+++ routes/whatsapp.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const router = express.Router();
+
+router.get("/webhook", (req, res) => {
+  const mode = req.query["hub.mode"];
+  const token = req.query["hub.verify_token"];
+  const challenge = req.query["hub.challenge"];
+
+  if (mode === "subscribe" && token === process.env.META_VERIFY_TOKEN) {
+    console.log("Webhook verified");
+    return res.status(200).send(challenge);
+  }
+  return res.sendStatus(403);
+});
+
+module.exports = router;
